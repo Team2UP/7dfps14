2d4ca123131ee65aec149ebc4a324500d82f5756
Adrian Corpuz <drianade15@gmail.com>
2014-11-11 07:53:04 +0800

    Applied assets and mountains!

--- Assets/scripts/CloudSpawn.cs
+++ Assets/scripts/CloudSpawn.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class CloudSpawn : MonoBehaviour
 {
-    private Object cloudRes;
+	private Object cloudRes1, cloudRes2, cloudRes3;
 
     void Awake()
     {
-        cloudRes = Resources.Load("prefabs/Cloud");
+		cloudRes1 = Resources.Load("prefabs/WhiteCloud1");
+		cloudRes2 = Resources.Load("prefabs/WhiteCloud2");
+		cloudRes3 = Resources.Load("prefabs/WhiteCloud3");
     }
 
     // Use this for initialization
@@ -23,7 +25,10 @@ public class CloudSpawn : MonoBehaviour
 			//yield return new WaitForSeconds(Random.Range(0.5f, 2f));
 			yield return new WaitForSeconds(Random.Range(1f, 3f));
 
-            Instantiate(cloudRes, transform.position, transform.rotation);
+			int randomCloud = Random.Range(1,4);
+			if(randomCloud == 1) Instantiate(cloudRes1, transform.position, transform.rotation);
+			else if(randomCloud == 2) Instantiate(cloudRes2, transform.position, transform.rotation);
+			else Instantiate(cloudRes3, transform.position, transform.rotation);
         }
     }
 }
---
+++ Assets/scripts/MountainSpawn.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MountainSpawn : MonoBehaviour {
+
+	private Object mountainRes1, mountainRes2;
+	
+	void Awake()
+	{
+		mountainRes1 = Resources.Load("prefabs/Mountain1");
+		mountainRes2 = Resources.Load("prefabs/Mountain2");
+	}
+	
+	// Use this for initialization
+	void Start()
+	{
+		StartCoroutine(SpawnMountain());
+	}
+	
+	IEnumerator SpawnMountain()
+	{
+		while (true)
+		{
+			//yield return new WaitForSeconds(Random.Range(0.5f, 2f));
+			yield return new WaitForSeconds(Random.Range(2f, 4f));
+			
+			int randomMountain = Random.Range(1,3);
+			if(randomMountain == 1) Instantiate(mountainRes1, transform.position, transform.rotation);
+			else Instantiate(mountainRes2, transform.position, transform.rotation);
+		}
+	}
+}
