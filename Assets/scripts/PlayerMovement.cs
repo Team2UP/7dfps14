6104e4ffda2ece69c35557ae8f69ed19de222546
nyrrrr <xnamespooferx@gmail.com>
2014-11-11 10:58:15 +0100

    Player Movement
    between x = -12 and 12 okay?

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
+    private float smooth = 50f;
+    Vector3 newPos;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +12,13 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetAxis("Horizontal") < 0 && transform.position.x > -12)
+        {
+            newPos = new Vector3(transform.position.x + 1f * Input.GetAxis("Horizontal"), transform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, newPos, smooth * Time.deltaTime);
+        } else if (Input.GetAxis("Horizontal") > 0 && transform.position.x < 12) {
+            newPos = new Vector3(transform.position.x + 1f * Input.GetAxis("Horizontal"), transform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, newPos, smooth * Time.deltaTime);
+        }
 	}
 }
