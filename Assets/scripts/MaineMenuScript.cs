d8bc2c5bd0be8939d509632c3a2495fb7f464188
Adrian Corpuz <drianade15@gmail.com>
2014-11-13 08:19:30 +0800

    Collision and Main Menu screen

---
+++ Assets/scripts/MaineMenuScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaineMenuScript : MonoBehaviour {
+	private float smooth = 50f;
+	Vector3 newPos;
+
+	bool moveLeft = false;
+	bool moveRight = false;
+	int position = 0;
+	
+	// Use this for initialization
+	void Start () {  
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.LeftArrow) && moveLeft == false) {
+			if (position <= -1) {
+				moveLeft = false;
+			} else {
+				moveLeft = true;
+				position--;
+			}
+		} else if (Input.GetKeyUp (KeyCode.RightArrow) && moveRight == false) {
+			if (position >= 1) {
+				moveRight = false;
+			} else {
+				moveRight = true;
+				position++;
+			}
+		} else if (Input.GetKeyUp (KeyCode.Space) && transform.position.x == 0) {
+			Application.LoadLevel(1);
+		}
+
+		if (moveLeft) {
+			if(transform.position.x <= (8f * position) ){
+				newPos = new Vector3((8f * position), transform.position.y, transform.position.z);
+				moveLeft = false;
+				transform.position = newPos;
+			} else {
+				newPos = new Vector3 (transform.position.x + 0.5f * -0.5f, transform.position.y, transform.position.z);
+				transform.position = Vector3.Lerp (transform.position, newPos, smooth * Time.deltaTime);
+			}
+		}
+		if (moveRight) {
+			if(transform.position.x >= (8f * position) ){
+				newPos = new Vector3((8f * position), transform.position.y, transform.position.z);
+				moveRight = false;
+				transform.position = newPos;
+			} else {
+				newPos = new Vector3 (transform.position.x + 0.5f * 0.5f, transform.position.y, transform.position.z);
+				transform.position = Vector3.Lerp (transform.position, newPos, smooth * Time.deltaTime);
+			}
+		}
+
+	}
+}
--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -6,18 +6,26 @@ public class PlayerMovement : MonoBehaviour {
     Vector3 newPos;
 
 	// Use this for initialization
-	void Start () {
+	void Start () {  
 	
 	}
+
+	void OnTriggerEnter2D(Collider2D col) 
+	{
+		if (col.transform.position.z <= transform.position.z) {
+			Debug.Log ("ENTER");
+		}
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetAxis("Horizontal") < 0 && transform.position.x > -12)
         {
-            newPos = new Vector3(transform.position.x + 1f * Input.GetAxis("Horizontal"), transform.position.y, transform.position.z);
+            newPos = new Vector3(transform.position.x + 0.5f * Input.GetAxis("Horizontal"), transform.position.y, transform.position.z);
             transform.position = Vector3.Lerp(transform.position, newPos, smooth * Time.deltaTime);
         } else if (Input.GetAxis("Horizontal") > 0 && transform.position.x < 12) {
-            newPos = new Vector3(transform.position.x + 1f * Input.GetAxis("Horizontal"), transform.position.y, transform.position.z);
+            newPos = new Vector3(transform.position.x + 0.5f * Input.GetAxis("Horizontal"), transform.position.y, transform.position.z);
             transform.position = Vector3.Lerp(transform.position, newPos, smooth * Time.deltaTime);
         }
 	}
